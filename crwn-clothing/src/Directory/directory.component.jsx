93f4e0b2572dc7e20bee424264b548fa4df7507c
Gh0stByt3s <orangageorge.gr@gmail.com>
2023-01-27 16:35:41 +0300

    Setting up the website components

---
+++ crwn-clothing/src/Directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from "../components/category-item/category-item-components"
+import './directory.styles.scss'
+
+const Directory = ({categories}) =>{
+    return(
+        <div className="directory-container">
+        {/* Looping through the categories object to display them one by one */}
+        {categories.map((category)=>(
+          // Passing the items and setting their key using their IDs one at a time to the category item component
+          < CategoryItem key={category.id} category={category}/>
+        ))}
+      </div>
+        
+    )
+}
+
+export default Directory
